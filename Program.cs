96d19ab21f9f99fe5a85303bcfc1b0fbd40ab42b
Alex <kenny480rus@mail.ru>
2023-11-29 23:03:11 +0300

    решили первую задачу

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int minNumber = 1;
+int maxNumber = 100;
+
+int minInTheRange = 20;
+int maxinTheRange = 90;
+int arraySize = 10;
+
+int[] arrayOfnumbers = new int[arraySize];
+Random rand = new Random();
+
+for (int i = 0; i < arraySize; i++)
+{
+    arrayOfnumbers[i] = rand.Next(minNumber, maxNumber);
+}
+
+Console.Write("В данном массиве [");
+
+int count = 0;
+for (int i = 0; i < arraySize; i++)
+{
+    if (i > 0)
+        Console.Write($", ");
+
+    int currentNumber = arrayOfnumbers[i];
+    if (currentNumber >= minInTheRange && currentNumber <= maxinTheRange)
+      ++count;
+    Console.Write($"{currentNumber}");
+   
+}
+
+Console.WriteLine($"] {count} чисел находится в диапазоне [{minInTheRange}, {maxinTheRange}]");
+
+// ЗАДАЧА 2
+
